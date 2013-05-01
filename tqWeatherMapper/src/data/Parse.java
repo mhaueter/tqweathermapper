fc2aabddd3c8d4fe4455cec717c6eb3d6b66681a
Michael Haueter <mhaueter334@gmail.com>
2013-04-30 21:50:33 -0500

    Completed parsing of mdf record lines for siteinfo

--- tqWeatherMapper/src/data/Parse.java
+++ tqWeatherMapper/src/data/Parse.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.ArrayList;
+
 /**
  * This Class allows for explicit parsing of data
  * 
@@ -10,11 +12,38 @@ package data;
 public class Parse {
 	public static String[] parseMdfRecord(String s){
 		char[] sarray = s.toCharArray();
+		ArrayList<String> newStrings = new ArrayList<String>();
+		newStrings.add("");
+		boolean inquote = false;
+		for(int i = 0; i < sarray.length; i++){
+			if(sarray[i] == ','){
+				newStrings.add("");
+			}
+			if((sarray[i] == '"') && (inquote == false)){
+				inquote = true;
+				continue;
+			}
+			if(inquote){
+				if(sarray[i] == '"'){
+					inquote = false;
+					continue;
+				}
+				newStrings.set(newStrings.size() - 1, newStrings.get(newStrings.size() - 1).concat(Character.toString(sarray[i])));
+			}
+			if(sarray[i] == ' '){
+				continue;
+			}
+			
+			
+			newStrings.set(newStrings.size() - 1, newStrings.get(newStrings.size() - 1).concat(Character.toString(sarray[i])));
+			
+		}
+		
+		String[] StringsArray = newStrings.toArray(new String[newStrings.size()]);
 		
 		
 		
 		
-		
-		return null;
+		return StringsArray;
 	}
 }
